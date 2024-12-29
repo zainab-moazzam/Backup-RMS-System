513eaa115bc022ace417ae9393e69a5471288c5a
zainab-moazzam <221095@students.au.edu.pk>
2024-12-30 01:31:04 +0500

    order section is completed footer will bi start

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ searchClose.onclick =() =>{
 // }
 
 var swiper = new Swiper(".home-slider", {
-    spaceBetween: 30,
+    spaceBetween: 200,
     centeredSlides: true,
     autoplay: {
         delay: 7500,
@@ -41,4 +41,27 @@ var swiper = new Swiper(".home-slider", {
         clickable: true,
     },
     loop:true,
+});
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    loop:true,
+    breakpoints: {
+    0: {
+        slidesPerView: 1,
+    },    
+    640:{
+        slidesPerView: 2,
+    },
+    768:{
+        slidesPerView: 2,
+    },
+    1024:{
+        slidesPerView: 3,
+    },
+    },
 });
