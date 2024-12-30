ddf7a64e8b4bedf91fce6e1a504499bcfc392c48
zainab-moazzam <221095@students.au.edu.pk>
2024-12-31 04:04:08 +0500

    all code structure is completed except the loader

--- script.js
+++ script.js
@@ -3,12 +3,29 @@ let navbar = document.querySelector('.navbar')
 
 menu.onclick = () =>{
     menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
+    navbar.classList.toggle('actives');
 }
+
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
 
 window.onscroll = () =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
+
+    section.forEach(sec =>{
+        let top =window.scrollY;
+        let height =sec.offsetHeight;
+        let offset =sec.offsetTop -150;
+        let id =sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height ){
+            navLinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+            });
+        };
+    });
 }
 
 let search = document.querySelector('#search-icon');
@@ -65,3 +82,20 @@ var swiper = new Swiper(".review-slider", {
     },
     },
 });
+// function loader() {
+//     document.querySelector('.loader-container').classList.add('fade-out');
+// }
+// function fadeOut() {
+//     setInterval(loader, 3000);
+// }
+// window.onload = fadeOut;
+function loader() {
+    const loaderContainer = document.querySelector('.loader-container');
+    loaderContainer.classList.add('fade-out');
+}
+
+function fadeOut() {
+    setTimeout(loader, 3000); // Wait 3 seconds before calling loader function
+}
+
+window.onload = fadeOut;  // Call fadeOut once the page has finished loading
